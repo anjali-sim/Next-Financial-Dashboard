edab7cb46b94b5ed6814d5580e37f5e587e3cf88
anjali-sim <anjalighetia123@gmail.com>
2024-02-29 10:43:34 +0530

    :card_file_box: Added the files for querying the data from the database and changed the dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,9 @@
+import { lusitana } from "../ui/fonts";
+
 export default function Page() {
-    return <p>Dashboard Page</p>;
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+        </main>
+    );
 }
